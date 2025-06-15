a3bdea6736cf97a5fd408949f74f5872594b0c7d
Peter Lycos <plycos@gmail.com>
2025-06-15 14:58:44 -0400

    update SqlRenderer

--- xqlite-java/src/main/java/dev/lycosp/xqlite/ast/SqlRenderer.java
+++ xqlite-java/src/main/java/dev/lycosp/xqlite/ast/SqlRenderer.java
@@ -1,12 +1,14 @@
 package dev.lycosp.xqlite.ast;
 
+import dev.lycosp.xqlite.ast.nodes.select.SelectNode;
 import dev.lycosp.xqlite.ast.visitor.SelectRenderVisitor;
 import dev.lycosp.xqlite.runtime.QuerySpec;
 
 public final class SqlRenderer {
-    private SqlRenderer() {}
+    private SqlRenderer() {
+    }
 
-    public static QuerySpec render(SqlNode node) {
+    public static QuerySpec render(SelectNode node) {
         return new SelectRenderVisitor().visit(node);
     }
 }
